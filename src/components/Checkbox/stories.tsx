3b95037a45ad3f9b7cb75c753329be4a400e10a2
Marcelo Lynx <marcelolynx@gmail.com>
2022-07-28 09:54:14 -0400

    adicionando componentes Radio e TextField

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -3,7 +3,38 @@ import Checkbox, { CheckboxProps } from '.'
 
 export default {
   title: 'Checkbox',
-  component: Checkbox
+  component: Checkbox,
+  argTypes: {
+    onCheck: { action: 'onCheck' }
+  }
 } as Meta
 
-export const Default: Story<CheckboxProps> = (args) => <Checkbox {...args} />
+export const Default: Story<CheckboxProps> = (args) => (
+  <>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Action"
+        labelFor="action"
+        isChecked
+        {...args}
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Adventure"
+        labelFor="adventure"
+        {...args}
+      />
+    </div>
+    <div style={{ padding: 10 }}>
+      <Checkbox
+        name="category"
+        label="Strategy"
+        labelFor="strategy"
+        {...args}
+      />
+    </div>
+  </>
+)
--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -1,6 +1,8 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import userEvent from '@testing-library/user-event'
 
+import theme from 'styles/theme'
 import Checkbox from '.'
 
 describe('<Checkbox />', () => {
@@ -21,5 +23,38 @@ describe('<Checkbox />', () => {
     renderWithTheme(<Checkbox />)
 
     expect(screen.queryByLabelText('Checkbox')).not.toBeInTheDocument()
+  })
+
+  it('should render with black label', () => {
+    renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" labelColor="black" />
+    )
+
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({
+      color: theme.colors.black
+    })
+  })
+
+  it('should dispatch onCheck when status changes', async () => {
+    const onCheck = jest.fn()
+    renderWithTheme(<Checkbox label="Checkbox" onCheck={onCheck} />)
+    expect(onCheck).not.toHaveBeenCalled()
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
+    })
+    expect(onCheck).toHaveBeenCalledWith(true)
+  })
+
+  it('should dispatch onCheck when status changes', async () => {
+    const onCheck = jest.fn()
+
+    renderWithTheme(<Checkbox label="Checkbox" onCheck={onCheck} isChecked />)
+
+    userEvent.click(screen.getByRole('checkbox'))
+    await waitFor(() => {
+      expect(onCheck).toHaveBeenCalledTimes(1)
+    })
+    expect(onCheck).toHaveBeenCalledWith(false)
   })
 })
