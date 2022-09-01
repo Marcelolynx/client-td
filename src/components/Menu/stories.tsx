7149c07c18164b98ba0a4ddda59f80f06e73ae70
Eduardo Pacheco <eduardopc91@hotmail.com>
2022-09-01 12:20:21 -0300

    fix: incorrect import on storybook file

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -1,12 +1,12 @@
 import { Story, Meta } from '@storybook/react'
-import Menu, { MenuProps } from '.'
+import Menu from '.'
 
 export default {
   title: 'Menu',
   component: Menu
 } as Meta
 
-export const Default: Story<MenuProps> = (args) => <Menu {...args} />
+export const Default: Story = () => <Menu />
 
 Default.parameters = {
   layout: 'fullscreen',
