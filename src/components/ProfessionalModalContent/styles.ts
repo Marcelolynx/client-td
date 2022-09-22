3ba25f99a4c33e2c7b1d34fb56f67751de8909d3
Eduardo Pacheco <eduardopc91@hotmail.com>
2022-09-21 23:48:32 -0300

    feat: add default profissional image when empty

--- src/components/ProfessionalModalContent/styles.ts
+++ src/components/ProfessionalModalContent/styles.ts
@@ -16,6 +16,7 @@ export const Wrapper = styled.div`
 export const ProfessionalInfo = styled.div`
   display: flex;
   flex-direction: row;
+  max-width: 600px;
 `
 
 export const ImageBox = styled.div`
@@ -23,13 +24,7 @@ export const ImageBox = styled.div`
     height: 40rem;
     width: 50%;
     min-width: 275px;
-    background-image: linear-gradient(
-      to right,
-      #f6f7f8 0%,
-      #edeef1 20%,
-      #f6f7f8 40%,
-      #f6f7f8 100%
-    );
+    background-image: ${theme.colors.white};
     background-size: 80rem 14rem;
     animation: placeholderShimmer 1s linear infinite forwards;
 
--- src/utils/getImageUrl.ts
+++ src/utils/getImageUrl.ts
@@ -1,4 +1,8 @@
 export const getImageUrl = (url: string | undefined) => {
+  if (url === undefined) {
+    return '/img/person.png'
+  }
+
   if (process.env.NEXT_PUBLIC_API_URL) {
     return `${process.env.NEXT_PUBLIC_API_URL}${url}`
   }
